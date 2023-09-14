8ef7c2cd1121b680303cdc75ec1d27e7754e1242
Cipher Codes <84612418+Cipher-Codes@users.noreply.github.com>
2023-09-14 22:08:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ window.onload = () => {
             .register('./sw.js');
     }
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e;
+});
+const installApp = document.getElementById('installApp');
+
+installApp.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    }
+});
